761894ae29e5e5ea650461ab4f36b22c6b605a03
KnightLeo <80933008+Knightleo602@users.noreply.github.com>
2022-05-19 17:50:58 -0300

    Add files via upload

--- script_supermercado.sql
+++ script_supermercado.sql
@@ -8,13 +8,20 @@ CREATE TABLE loja(
 	primary key (ID_loja)
 );
 
+CREATE TABLE cargo(
+	ID_cargo int NOT NULL AUTO_INCREMENT,
+	descricao varchar(100),
+	primary key (ID_cargo)
+);
+
 CREATE TABLE funcionario(
 	ID_func int NOT NULL AUTO_INCREMENT,
 	nome varchar(100),
 	email varchar(100),
 	salario int,
-	profissao varchar(100),
-	primary key (ID_func)
+	cargo int,
+	primary key (ID_func),
+	foreign key (cargo) references cargo(ID_cargo)
 );
 
 CREATE TABLE funcionario_loja(
@@ -25,9 +32,16 @@ CREATE TABLE funcionario_loja(
 );
 
 CREATE TABLE dependentes(
-	ID_func int, 
+	ID_depe int NOT NULL AUTO_INCREMENT, 
 	nome varchar(100),
-	foreign key (ID_func) references funcionario(ID_func)
+	primary key (ID_depe)
+);
+
+CREATE TABLE funcionario_dependente(
+	ID_func int,
+	ID_depe int,
+	foreign key (ID_func) references funcionario(ID_func),
+	foreign key (ID_depe) references dependentes(ID_depe)
 );
 
 CREATE TABLE departamento(
@@ -84,10 +98,12 @@ CREATE TABLE cliente(
 CREATE TABLE compras(
 	ID_prod int,
 	ID_cli int,
+	ID_loja int,
 	data_compra date,
 	qntd int,
 	foreign key (ID_prod) references produto(ID_prod),
-	foreign key (ID_cli) references cliente(ID_cli)
+	foreign key (ID_cli) references cliente(ID_cli),
+	foreign key (ID_loja) references loja(ID_loja)
 );
 
 ### POPULACAO DO BANCO DE DADOS
@@ -106,17 +122,31 @@ INSERT INTO cliente VALUES (NULL, "matthew", "batata", "aaaa@bbbb.com", "rua nao
 INSERT INTO cliente VALUES (NULL, "knight", "senha", "email@email.com", "rua rua", "987421543", "85346523412");
 INSERT INTO cliente VALUES (NULL, "sim", "aiai", "muito@massa.com", "rua sim", "912341234", "98765432100");
 
+# adicionando cargos:
+#(id, nome/descricao)
+INSERT INTO cargo VALUES (1, "gerente");
+INSERT INTO cargo VALUES (2, "estoqueiro");
+INSERT INTO cargo VALUES (3, "confeiteiro");
+INSERT INTO cargo VALUES (4, "açougueiro");
+
 # adicionando funcionarios:
-#(id, nome, email, salario, profissao)
-INSERT INTO funcionario VALUES (NULL, "cara", "cara@znl.com", 1500, "estoqueiro");
-INSERT INTO funcionario VALUES (NULL, "joni", "joni233223@znl.com", 5600, "gerente");
-INSERT INTO funcionario VALUES (NULL, "mari", "maaari@znl.com", 2100, "confeiteira");
-INSERT INTO funcionario VALUES (NULL, "mario", "mwahahah@ser.com", 2500, "açougueiro");
+#(id, nome, email, salario, idcargo)
+INSERT INTO funcionario VALUES (NULL, "cara", "cara@znl.com", 1500, 1);
+INSERT INTO funcionario VALUES (NULL, "joni", "joni233223@znl.com", 5600, 2);
+INSERT INTO funcionario VALUES (NULL, "mari", "maaari@znl.com", 2100, 3);
+INSERT INTO funcionario VALUES (NULL, "mario", "mwahahah@ser.com", 2500, 4);
 
 # adicionando dependentes 
 #(id, endereco)
-INSERT INTO dependentes VALUES (2, "jonas");
-INSERT INTO dependentes VALUES (4, "luigi");
+INSERT INTO dependentes VALUES (NULL, "jonas");
+INSERT INTO dependentes VALUES (NULL, "jonus");
+INSERT INTO dependentes VALUES (NULL, "luigi");
+
+# adicionando relacao funcionarios dependentes 
+#(idfunc, iddepe)
+INSERT INTO funcionario_dependente VALUES (2, 1);
+INSERT INTO funcionario_dependente VALUES (2, 2);
+INSERT INTO funcionario_dependente VALUES (4, 3);
 
 # adicionando funcionarios que trabalham na loja (dependendo da funcao pode trabalhar em mais que um):
 #(id loja, id func)
@@ -137,7 +167,6 @@ INSERT INTO departamento VALUES (5, "alimentos");
 #(id loja, id depto)
 INSERT INTO loja_departamento VALUES (1, 1);
 INSERT INTO loja_departamento VALUES (1, 2);
-INSERT INTO loja_departamento VALUES (1, 3);
 INSERT INTO loja_departamento VALUES (1, 4);
 INSERT INTO loja_departamento VALUES (2, 3);
 
@@ -155,13 +184,16 @@ INSERT INTO produto VALUES (9, "Gelatina de morando", 9, 30, "2025-01-01", 5);
 INSERT INTO produto VALUES (10, "Regata branca", 20, 25, NULL, 3);
 
 # adicionando as compras que os clientes fizeram:
-#(id prod, id cliente, data, qntd)
-INSERT INTO compras VALUES (1, 1, "2022-04-09", 1);
-INSERT INTO compras VALUES (4, 1, "2022-04-09", 1);
-INSERT INTO compras VALUES (6, 3, "2020-06-16", 1);
-INSERT INTO compras VALUES (3, 2, "2022-04-20", 3);
-INSERT INTO compras VALUES (8, 2, "2022-04-20", 2);
-INSERT INTO compras VALUES (9, 2, "2022-04-20", 5);
+#(id prod, id cliente, id loja, data, qntd)
+INSERT INTO compras VALUES (1, 1, 1, "2022-04-09", 2);
+INSERT INTO compras VALUES (2, 1, 1, "2022-04-09", 1);
+INSERT INTO compras VALUES (2, 2, 1, "2022-02-22", 1);
+INSERT INTO compras VALUES (4, 1, 1, "2022-04-09", 1);
+INSERT INTO compras VALUES (6, 3, 1, "2020-06-16", 1);
+INSERT INTO compras VALUES (3, 2, 1, "2022-04-20", 3);
+INSERT INTO compras VALUES (8, 2, 2, "2022-04-20", 2);
+INSERT INTO compras VALUES (9, 2, 2, "2022-04-20", 5);
+INSERT INTO compras VALUES (6, 1, 1, "2021-07-20", 1);
 
 # adicionando fornecedores:
 #(id, nome, qntd, data fornecido, valor)
