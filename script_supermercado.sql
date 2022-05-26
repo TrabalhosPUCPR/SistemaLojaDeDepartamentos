d34b25ad4d9bca8d98b5a58618ed36ec7f108878
KnightLeo <80933008+Knightleo602@users.noreply.github.com>
2022-05-26 14:27:21 -0300

    optimizacoes e select diferente
    select numero 15 foi mudado pois era muito parecido com uma outra

--- script_supermercado.sql
+++ script_supermercado.sql
@@ -1,4 +1,4 @@
-DROP DATABASE MercadaoShow ;
+DROP DATABASE IF EXISTS MercadaoShow ;
 CREATE DATABASE MercadaoShow;
 USE MercadaoShow;
 
@@ -16,17 +16,17 @@ CREATE TABLE cargo(
 
 CREATE TABLE funcionario(
 	ID_func int NOT NULL AUTO_INCREMENT,
-	nome varchar(100),
+	nome varchar(100) NOT NULL,
 	email varchar(100),
 	salario int,
-	cargo int,
+	cargo int NOT NULL,
 	primary key (ID_func),
 	foreign key (cargo) references cargo(ID_cargo)
 );
 
 CREATE TABLE funcionario_loja(
-	ID_loja int,
-	ID_func int,
+	ID_loja int NOT NULL,
+	ID_func int NOT NULL,
 	foreign key (ID_loja) references loja(ID_loja),
 	foreign key (ID_func) references funcionario(ID_func)
 );
@@ -38,8 +38,8 @@ CREATE TABLE dependentes(
 );
 
 CREATE TABLE funcionario_dependente(
-	ID_func int,
-	ID_depe int,
+	ID_func int NOT NULL,
+	ID_depe int NOT NULL,
 	foreign key (ID_func) references funcionario(ID_func),
 	foreign key (ID_depe) references dependentes(ID_depe)
 );
@@ -51,8 +51,8 @@ CREATE TABLE departamento(
 );
 
 CREATE TABLE loja_departamento(
-	ID_loja int,
-	ID_dpto int,
+	ID_loja int NOT NULL,
+	ID_dpto int NOT NULL,
 	foreign key (ID_loja) references loja(ID_loja),
 	foreign key (ID_dpto) references departamento (ID_dpto)
 );
@@ -63,7 +63,7 @@ CREATE TABLE produto(
 	valor int, 
 	qntd int,
 	validade date,
-	ID_dpto int,
+	ID_dpto int NOT NULL,
 	primary key (ID_prod),
 	foreign key (ID_dpto) references departamento(ID_dpto)
 );
@@ -75,8 +75,8 @@ CREATE TABLE fornecedor(
 );
 
 CREATE TABLE produto_fornecedor(
-	ID_prod int,
-	ID_for int,
+	ID_prod int NOT NULL,
+	ID_for int NOT NULL,
 	valor_entrega int,
 	data_for date,
 	qntd int,
@@ -96,9 +96,9 @@ CREATE TABLE cliente(
 );
 
 CREATE TABLE compras(
-	ID_prod int,
-	ID_cli int,
-	ID_loja int,
+	ID_prod int NOT NULL,
+	ID_cli int NOT NULL,
+	ID_loja int NOT NULL,
 	data_compra date,
 	qntd int,
 	foreign key (ID_prod) references produto(ID_prod),
@@ -112,110 +112,122 @@ CREATE TABLE compras(
 
 # adicionando lojas
 #(id, endereco)
-INSERT INTO loja VALUES (NULL, "rua muito legal");
-INSERT INTO loja VALUES (NULL, "rua sim");
+INSERT INTO loja VALUES 
+(NULL, "rua muito legal"),
+(NULL, "rua sim");
 
 # adicionando clientes:
 #(id, nome, senha, email, endereco, telefone, cpf(11 digitos))
-INSERT INTO cliente VALUES (NULL, "leo", "bomdia123", "leo@leo123.com", "rua muito legal", "992334455", "12345678910");
-INSERT INTO cliente VALUES (NULL, "matthew", "batata", "aaaa@bbbb.com", "rua nao muito legal", "991314686", "35613458734");
-INSERT INTO cliente VALUES (NULL, "knight", "senha", "email@email.com", "rua rua", "987421543", "85346523412");
-INSERT INTO cliente VALUES (NULL, "sim", "aiai", "muito@massa.com", "rua sim", "912341234", "98765432100");
+INSERT INTO cliente VALUES 
+(NULL, "leo", "bomdia123", "leo@leo123.com", "rua muito legal", "992334455", "12345678910"),
+(NULL, "matthew", "batata", "aaaa@bbbb.com", "rua nao muito legal", "991314686", "35613458734"),
+(NULL, "knight", "senha", "email@email.com", "rua rua", "987421543", "85346523412"),
+(NULL, "sim", "aiai", "muito@massa.com", "rua sim", "912341234", "98765432100");
 
 # adicionando cargos:
 #(id, nome/descricao)
-INSERT INTO cargo VALUES (1, "gerente");
-INSERT INTO cargo VALUES (2, "estoqueiro");
-INSERT INTO cargo VALUES (3, "confeiteiro");
-INSERT INTO cargo VALUES (4, "açougueiro");
+INSERT INTO cargo VALUES 
+(1, "gerente"),
+(2, "estoqueiro"),
+(3, "confeiteiro"),
+(4, "açougueiro");
 
 # adicionando funcionarios:
 #(id, nome, email, salario, idcargo)
-INSERT INTO funcionario VALUES (NULL, "cara", "cara@znl.com", 1500, 1);
-INSERT INTO funcionario VALUES (NULL, "joni", "joni233223@znl.com", 5600, 2);
-INSERT INTO funcionario VALUES (NULL, "mari", "maaari@znl.com", 2100, 3);
-INSERT INTO funcionario VALUES (NULL, "mario", "mwahahah@ser.com", 2500, 4);
+INSERT INTO funcionario VALUES 
+(NULL, "cara", "cara@znl.com", 1500, 1),
+(NULL, "joni", "joni233223@znl.com", 5600, 2),
+(NULL, "mari", "maaari@znl.com", 2100, 3),
+(NULL, "mario", "mwahahah@ser.com", 2500, 4);
 
 # adicionando dependentes 
 #(id, endereco)
-INSERT INTO dependentes VALUES (NULL, "jonas");
-INSERT INTO dependentes VALUES (NULL, "jonus");
-INSERT INTO dependentes VALUES (NULL, "luigi");
+INSERT INTO dependentes VALUES 
+(NULL, "jonas"),
+(NULL, "jonus"),
+(NULL, "luigi");
 
 # adicionando relacao funcionarios dependentes 
 #(idfunc, iddepe)
-INSERT INTO funcionario_dependente VALUES (2, 1);
-INSERT INTO funcionario_dependente VALUES (2, 2);
-INSERT INTO funcionario_dependente VALUES (4, 3);
+INSERT INTO funcionario_dependente VALUES 
+(2, 1),
+(2, 2),
+(4, 3);
 
 # adicionando funcionarios que trabalham na loja (dependendo da funcao pode trabalhar em mais que um):
 #(id loja, id func)
-INSERT INTO funcionario_loja VALUES (1, 2);
-INSERT INTO funcionario_loja VALUES (1, 1);
-INSERT INTO funcionario_loja VALUES (1, 3);
-INSERT INTO funcionario_loja VALUES (2, 2);
+INSERT INTO funcionario_loja VALUES 
+(1, 2),
+(1, 1),
+(1, 3),
+(2, 2);
 
 # adicionando departamentos:
 #(id, nome)
-INSERT INTO departamento VALUES (1, "eletronicos");
-INSERT INTO departamento VALUES (2, "brinquedos");
-INSERT INTO departamento VALUES (3, "vestuarios");
-INSERT INTO departamento VALUES (4, "mobiliario");
-INSERT INTO departamento VALUES (5, "alimentos");
+INSERT INTO departamento VALUES (1, "eletronicos"),
+(2, "brinquedos"),
+(3, "vestuarios"),
+(4, "mobiliario"),
+(5, "alimentos");
 
 # adicionando departamentos que estao em quais lojas:
 #(id loja, id depto)
-INSERT INTO loja_departamento VALUES (1, 1);
-INSERT INTO loja_departamento VALUES (1, 2);
-INSERT INTO loja_departamento VALUES (1, 4);
-INSERT INTO loja_departamento VALUES (2, 3);
+INSERT INTO loja_departamento VALUES 
+(1, 1),
+(1, 2),
+(1, 4),
+(2, 3);
 
 # adicionando produtos:
 #(id, nome, valor, qntd, validade, ID_dep, tipo)
-INSERT INTO produto VALUES (1, "televisao 4k 80 polegadas OLED LG", 1000, 15, NULL, 1);
-INSERT INTO produto VALUES (2, "Earbuds bluetooth", 120, 22, NULL, 1);
-INSERT INTO produto VALUES (3, "Sucrilhos Kelloggs", 10, 100, "2022-04-30", 5);
-INSERT INTO produto VALUES (4, "Fruit Loops", 15, 100, "2022-05-22", 5);
-INSERT INTO produto VALUES (5, "Mesa de escritorio", 410, 10, NULL, 4);
-INSERT INTO produto VALUES (6, "Cadeira gamer", 980, 5, NULL, 4);
-INSERT INTO produto VALUES (7, "Banco de plastico", 39, 25, NULL, 4);
-INSERT INTO produto VALUES (8, "Pasta de amendoim", 9, 51, "2025-01-01", 5);
-INSERT INTO produto VALUES (9, "Gelatina de morando", 9, 30, "2025-01-01", 5);
-INSERT INTO produto VALUES (10, "Regata branca", 20, 25, NULL, 3);
+INSERT INTO produto VALUES 
+(1, "televisao 4k 80 polegadas OLED LG", 1000, 15, NULL, 1),
+(2, "Earbuds bluetooth", 120, 22, NULL, 1),
+(3, "Sucrilhos Kelloggs", 10, 100, "2022-04-30", 5),
+(4, "Fruit Loops", 15, 100, "2022-05-22", 5),
+(5, "Mesa de escritorio", 410, 10, NULL, 4),
+(6, "Cadeira gamer", 980, 5, NULL, 4),
+(7, "Banco de plastico", 39, 25, NULL, 4),
+(8, "Pasta de amendoim", 9, 51, "2025-01-01", 5),
+(9, "Gelatina de morando", 9, 30, "2025-01-01", 5),
+(10, "Regata branca", 20, 25, NULL, 3);
 
 # adicionando as compras que os clientes fizeram:
 #(id prod, id cliente, id loja, data, qntd)
-INSERT INTO compras VALUES (1, 1, 1, "2022-04-09", 2);
-INSERT INTO compras VALUES (2, 1, 1, "2022-04-09", 1);
-INSERT INTO compras VALUES (2, 2, 1, "2022-02-22", 1);
-INSERT INTO compras VALUES (4, 1, 1, "2022-04-09", 1);
-INSERT INTO compras VALUES (6, 3, 1, "2020-06-16", 1);
-INSERT INTO compras VALUES (3, 2, 1, "2022-04-20", 3);
-INSERT INTO compras VALUES (8, 2, 2, "2022-04-20", 2);
-INSERT INTO compras VALUES (9, 2, 2, "2022-04-20", 5);
-INSERT INTO compras VALUES (6, 1, 1, "2021-07-20", 1);
+INSERT INTO compras VALUES 
+(1, 1, 1, "2022-04-09", 2),
+(2, 1, 1, "2022-04-09", 1),
+(2, 2, 1, "2022-02-22", 1),
+(4, 1, 1, "2022-04-09", 1),
+(6, 3, 1, "2020-06-16", 1),
+(3, 2, 1, "2022-04-20", 3),
+(8, 2, 2, "2022-04-20", 2),
+(9, 2, 2, "2022-04-20", 5),
+(6, 1, 1, "2021-07-20", 1);
 
 # adicionando fornecedores:
 #(id, nome, qntd, data fornecido, valor)
-INSERT INTO fornecedor VALUES (1, "Super eletronicos show");
-INSERT INTO fornecedor VALUES (2, "Comidas 9000");
-INSERT INTO fornecedor VALUES (3, "Irmao mobiliarios");
-INSERT INTO fornecedor VALUES (4, "So no compiuter");
+INSERT INTO fornecedor VALUES 
+(1, "Super eletronicos show"),
+(2, "Comidas 9000"),
+(3, "Irmao mobiliarios"),
+(4, "So no compiuter");
 
 # adicionando produtos fornecidos pelos fornecedores:
 #(id produto, id fornecedor, valor da entrega, data da entrega, qntd)
-INSERT INTO produto_fornecedor VALUES (1, 1, 2000, "2022-01-23", 10);
-INSERT INTO produto_fornecedor VALUES (2, 1, 500, "2022-01-23", 10);
-INSERT INTO produto_fornecedor VALUES (3, 2, 250, "2022-03-07", 43);
-INSERT INTO produto_fornecedor VALUES (4, 2, 250, "2022-03-07", 66);
-INSERT INTO produto_fornecedor VALUES (8, 2, 150, "2022-04-15", 25);
-INSERT INTO produto_fornecedor VALUES (9, 2, 150, "2022-04-15", 25);
-INSERT INTO produto_fornecedor VALUES (5, 3, 1400, "2022-04-15", 5);
-INSERT INTO produto_fornecedor VALUES (6, 3, 1400, "2022-04-15", 5);
-INSERT INTO produto_fornecedor VALUES (7, 3, 200, "2022-04-15", 15);
-INSERT INTO produto_fornecedor VALUES (2, 4, 370, "2021-12-28", 6);
-INSERT INTO produto_fornecedor VALUES (6, 4, 980, "2021-12-28", 5);
-INSERT INTO produto_fornecedor VALUES (1, 4, 1500, "2021-12-28", 6);
+INSERT INTO produto_fornecedor VALUES 
+(1, 1, 2000, "2022-01-23", 10),
+(2, 1, 500, "2022-01-23", 10),
+(3, 2, 250, "2022-03-07", 43),
+(4, 2, 250, "2022-03-07", 66),
+(8, 2, 150, "2022-04-15", 25),
+(9, 2, 150, "2022-04-15", 25),
+(5, 3, 1400, "2022-04-15", 5),
+(6, 3, 1400, "2022-04-15", 5),
+(7, 3, 200, "2022-04-15", 15),
+(2, 4, 370, "2021-12-28", 6),
+(6, 4, 980, "2021-12-28", 5),
+(1, 4, 1500, "2021-12-28", 6);
 
 
 
--- selects.sql
+++ selects.sql
@@ -2,20 +2,20 @@
 
 # 1 Quantos funcionários trabalham em uma loja
 SELECT COUNT(*) as quantidade FROM funcionario_loja
-WHERE ID_loja = 1
+WHERE ID_loja = 1;
 
 # 2 Quantos dependentes o funcionário tem
 SELECT nome, COUNT(*) as quantidade FROM funcionario
 INNER JOIN funcionario_dependente ON funcionario.ID_func = funcionario_dependente.ID_func 
 WHERE nome = "joni"
-GROUP BY nome
+GROUP BY nome;
 
 # 3 Quanto é gasto pela empresa em salário dos funcionários
-SELECT SUM(salario) as Total FROM funcionario
+SELECT SUM(salario) as Total FROM funcionario;
 
 # 4 Qual a quantidade de funcionários que são de uma específica profissão
 SELECT COUNT(*) as quantidade FROM cargo
-WHERE descricao = "estoqueiro"
+WHERE descricao = "estoqueiro";
 
 # 5 Qual loja tem a maior quantidade de compras durante todo seu tempo de vida ou em uma data determinada
 SELECT endereco, COUNT(*) as quantidade FROM loja
@@ -23,33 +23,33 @@ INNER JOIN compras ON compras.ID_loja = loja.ID_loja
 WHERE compras.data_compra BETWEEN '2020-01-01' AND '2022-04-10' AND loja.ID_loja = 1# so comentar essa linha para mostrar de todo o seu tempo de vida
 GROUP BY endereco 
 ORDER BY quantidade DESC 
-LIMIT 1
+LIMIT 1;
 
 # 6 Quais departamentos são os mais lucrativos (nesse caso vai ser os que tem o maior valor na soma do preco dos produtos vezes a quantidade do estoque)
 SELECT departamento.nome, SUM(produto.valor*produto.qntd) as total FROM departamento, produto
 WHERE departamento.ID_dpto = produto.ID_dpto 
 GROUP BY departamento.nome
 ORDER BY total DESC
-LIMIT 1
+LIMIT 1;
 
 # 7 Em quais locais possui uma loja com um departamento especifico
 SELECT endereco FROM loja 
 INNER JOIN loja_departamento on loja.ID_loja = loja_departamento.ID_loja 
-WHERE loja_departamento.ID_dpto = 3
+WHERE loja_departamento.ID_dpto = 3;
 
 # 8 Quantos produtos cada departamento tem a venda (cada produto diferente, nao adiciona a quantidade que tem de cada)
 SELECT departamento.nome, COUNT(*) as quantidade FROM departamento, produto
 WHERE departamento.ID_dpto = produto.ID_dpto 
-GROUP BY nome
+GROUP BY nome;
 
 # 9 Quais produtos devem ser trocados por ter passado da data de validade
 SELECT nome, validade FROM produto
-WHERE CURRENT_DATE() > validade 
+WHERE CURRENT_DATE() > validade;
 
 # 10 Quantos produtos foram vendidos durante uma data
 SELECT COUNT(*) as quantidade_produtos_unico, SUM(compras.qntd) as quantidade_total FROM produto
 INNER JOIN compras ON produto.ID_prod = compras.ID_prod
-WHERE compras.data_compra BETWEEN '2022-01-01' AND '2022-05-05'
+WHERE compras.data_compra BETWEEN '2022-01-01' AND '2022-05-05';
 
 # 11 Qual produto é o mais vendido de todos ou de apenas um departamento
 SELECT produto.nome, SUM(compras.qntd) as total FROM produto
@@ -57,46 +57,45 @@ INNER JOIN compras ON produto.ID_prod = compras.ID_prod
 WHERE produto.ID_dpto = 1 # so tirar essa linha para ver de todos
 GROUP BY produto.nome 
 ORDER BY total DESC 
-LIMIT 1
+LIMIT 1;
 
 # 12 Qual é o lucro de um produto em relação ao preço de venda e o preço para o fornecimento
 SELECT produto.nome, produto.valor*produto.qntd-produto_fornecedor.valor_entrega FROM produto, produto_fornecedor
 WHERE produto.ID_prod = 1
-GROUP BY produto.nome
+GROUP BY produto.nome;
 
 # 13 Quantidade de produtos vendidos em uma determinada data em uma loja especifica e em um departamento especifico e o quanto foi recebido nas vendas
 SELECT SUM(compras.qntd) as quantidade_de_produtos, SUM(produto.valor*compras.qntd) as valor_total FROM compras
 INNER JOIN produto ON compras.ID_prod = produto.ID_prod 
 INNER JOIN departamento ON produto.ID_dpto = departamento.ID_dpto 
 INNER JOIN loja ON compras.ID_loja = loja.ID_loja 
-WHERE compras.data_compra BETWEEN '2022-02-01' AND '2022-03-01' AND loja.ID_loja = 1 AND departamento.nome = "eletronicos"
+WHERE compras.data_compra BETWEEN '2022-02-01' AND '2022-03-01' AND loja.ID_loja = 1 AND departamento.nome = "eletronicos";
 
 # 14 Quanto foi gasto no total com fornecedores
-SELECT sum(valor_entrega) as total FROM produto_fornecedor
+SELECT sum(valor_entrega) as total FROM produto_fornecedor;
 
-# 15 Quantas compras foram feitas em uma data determinada
-SELECT COUNT(*) as total FROM compras
-WHERE data_compra BETWEEN '2022-02-01' AND '2022-02-31'
+# 15 Quantos fornecedores o sistema tem
+SELECT COUNT(*) as quantidade FROM fornecedor;
 
 # 16 Quais as compras que um cliente fez numa data determinada
 SELECT cliente.nome, produto.nome, loja.endereco, compras.data_compra, compras.qntd FROM cliente
 INNER JOIN compras ON cliente.ID_cli = compras.ID_cli
 INNER JOIN produto ON produto.ID_prod = compras.ID_prod 
 INNER JOIN loja ON loja.ID_loja = compras.ID_loja 
-WHERE cliente.nome = "leo" AND compras.data_compra BETWEEN '2020-01-01' AND '2022-01-01'
+WHERE cliente.nome = "leo" AND compras.data_compra BETWEEN '2020-01-01' AND '2022-01-01';
 
 # 17 Qual o local com a maior quantidade de clientes em geral
 SELECT COUNT(*) as quantidade, endereco FROM cliente
 GROUP BY endereco
 ORDER BY quantidade DESC 
-LIMIT 1
+LIMIT 1;
 
 # 18 Quanto que um cliente gastou em uma determinada data
 SELECT SUM(produto.valor*compras.qntd) as valor FROM compras
 INNER JOIN produto ON compras.ID_prod = produto.ID_prod 
 INNER JOIN cliente ON cliente.ID_cli = compras.ID_cli 
 WHERE cliente.nome = "leo" AND data_compra BETWEEN '2021-02-01' AND '2022-02-31'
-GROUP BY cliente.nome 
+GROUP BY cliente.nome;
 
 # 19 Qual dos fornecedores tem a melhor oferta em relação a quantidade e preço do produto para o fornecimento.
 SELECT fornecedor.nome as nome_fornecedor, produto_fornecedor.valor_entrega/produto_fornecedor.qntd as valor FROM produto_fornecedor
@@ -104,10 +103,10 @@ INNER JOIN produto ON produto_fornecedor.ID_prod = produto.ID_prod
 INNER JOIN fornecedor ON produto_fornecedor.ID_for = fornecedor.ID_for 
 WHERE produto.nome = "cadeira gamer"
 ORDER BY valor ASC
-LIMIT 1
+LIMIT 1;
 
 # 20 Quantos clientes foram cadastrados no sistema
-SELECT COUNT(*) as quantidade FROM cliente
+SELECT COUNT(*) as quantidade FROM cliente;
 
 
 
